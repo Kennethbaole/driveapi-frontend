e0ccf6e64d2961e83e384b2ed20e0fb4914138a2
KennethBaoLe <kennethble@gmail.com>
2026-03-12 22:25:03 -0400

    oops again, typo

--- lib/api.ts
+++ lib/api.ts
@@ -55,21 +55,21 @@ export async function createBooking(vehicleId: number, startDate: string, endDat
 }
 
 export async function createVehicle(data: any) {
-    return fetchAPI('/vehicle', {
+    return fetchAPI('/vehicles', {
         method: 'POST',
         body: JSON.stringify(data), 
     })
 }
 
 export async function updateVehicle(id: number, data: any) {
-    return fetchAPI(`/vehicle/${id}`, {
+    return fetchAPI(`/vehicles/${id}`, {
         method: 'PUT',
         body: JSON.stringify(data), 
     })
 }
 
 export async function deleteVehicle(id: Number) {
-    return fetchAPI(`/vehicle/${id}`, {
+    return fetchAPI(`/vehicles/${id}`, {
         method: 'DELETE', 
     })
 }
